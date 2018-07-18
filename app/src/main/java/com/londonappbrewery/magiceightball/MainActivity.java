81b5b0c8121e094dbf156da960a9cd24ad7ea5d0
syed.irfanullah <irfansyed.pg@gmail.com>
2018-07-18 12:11:16 +0500

    irfan syed commitment

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,45 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    int[] arryImage={
+            R.drawable.ball1,
+            R.drawable.ball2,
+            R.drawable.ball3,
+            R.drawable.ball4,
+            R.drawable.ball5
+    };
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         img=(ImageView)findViewById(R.id.img);
+        Button btn=(Button)findViewById(R.id.btn_);
+
+
+
+        btn.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Random rdm=new Random();
+        int number=rdm.nextInt(5);
+
+        img.setImageResource(arryImage[number]);
+
+
     }
 }
